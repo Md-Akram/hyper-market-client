102c1c2b80c7d7a89177d1d058eabe848cf16b4d
md-akram <akramkaikobad@gmail.com>
2023-11-08 02:13:04 +0600

    added FAQ section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from '../components/Banner'
 import BrowseByCategory from '../components/BrowseByCategory'
 import Reviews from '../components/Reviews'
+import FAQs from '../components/FAQs'
 
 const Home = () => {
     return (<>
         <Banner />
         <BrowseByCategory />
         <Reviews />
+        <FAQs />
     </>
     )
 }
